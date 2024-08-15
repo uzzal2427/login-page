866698c96b0f6a9bdee861346d7c0368b9e95fc5
uzzal2427 <uzzal2427ahmed@gmail.com>
2024-08-16 02:08:51 +0300

    page design complete

--- src/components/pages/login/Register.jsx
+++ src/components/pages/login/Register.jsx
@@ -1,5 +1,71 @@
+import { Link } from "react-router-dom";
+import img from "../../../assets/img2.webp";
+
 const Register = () => {
-  return <div>this is register page</div>;
+  return (
+    <div
+      className="hero min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="hero-content flex-col lg:flex-col w-full">
+        <div className="card bg-white w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="text-center lg:text-left text-black">
+              <h1 className="text-5xl font-bold">Register now!</h1>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div>
+              <p>
+                Already Have Account{" "}
+                <Link className="underline text-gray-600" to="/login">
+                  Please Login
+                </Link>
+              </p>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
